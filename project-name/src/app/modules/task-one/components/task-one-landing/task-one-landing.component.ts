208414937c2cf476abd9a48c0fa49c4cb0ea313b
saikatkundu27 <saikatrajkundu@gmail.com>
2023-11-09 00:56:05 +0530

    json file added

--- project-name/src/app/modules/task-one/components/task-one-landing/task-one-landing.component.ts
+++ project-name/src/app/modules/task-one/components/task-one-landing/task-one-landing.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { ModalService } from '../../services/modal.service';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
